597bdcf2b759933da774ae4bdb79c280b0325f9b
JuanSaldana1 <72858838+JuanSaldana1@users.noreply.github.com>
2022-02-21 18:00:41 +0100

    Finished app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'A511';
+  title = "A511";
+
+  showNotification() {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission();
+    } else {
+      var notification = new Notification("Título de la notificación", {
+        icon: "../assets/android-chrome-192x192.png",
+        body: "Este es el cuerpoi de la notificación",
+      });
+      notification.onclick = function () {
+        window.open("https://google.es");
+      };
+    }
+  }
 }
